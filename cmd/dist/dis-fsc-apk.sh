298b99ebab5e1e3aebb064c9568db722b91db2c2
laborc <laborc2009@gmail.com>
2015-05-25 18:25:13 -0700

    new file:   cmd/dist/dis-fsc-apk.sh

---
+++ cmd/dist/dis-fsc-apk.sh
@@ -0,0 +1,3 @@
+#!/bin/sh 
+lftp -u devftp,devftp 115.231.93.40 -e 'put /home/ftp/dev/apps/fsc-android-1.0-SNAPSHOT-prod.apk -o apps/fsc-android-1.0-SNAPSHOT-prod.apk; quit'
+lftp -u devftp,devftp 183.203.18.61 -e 'put /home/ftp/dev/apps/fsc-android-1.0-SNAPSHOT-jz.apk -o apps/fsc-android-1.0-SNAPSHOT-jz.apk; quit'
