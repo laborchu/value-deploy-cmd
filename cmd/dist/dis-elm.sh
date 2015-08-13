7710172084ca8d952f2fda942d37354e57695e7e
laborc <laborc2009@gmail.com>
2015-08-13 15:17:05 +0800

    deleted:    cmd/deploy/fsc-nodejs/fsc-nodejs-deploy.sh 	modified:   cmd/dist/dis-elm.sh 	deleted:    cmd/dist/dis-fsc-nodejs.sh 	deleted:    cmd/dist/fsc-nodejs.sh 	cmd/deploy/elos-nodejs/ 	cmd/dist/dis-elos-nodejs.sh

--- cmd/dist/dis-elm.sh
+++ cmd/dist/dis-elm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
-lftp -u devftp,devftp 61.153.97.56 -e 'put /home/ftp/dev/apps/elm.tar.gz -o apps/elm.tar.gz; quit'
+#lftp -u devftp,devftp 61.153.97.56 -e 'put /home/ftp/dev/apps/elm.tar.gz -o apps/elm.tar.gz; quit'
 lftp -u devftp,devftp 183.203.18.61 -e 'put /home/ftp/dev/apps/elm.tar.gz -o apps/elm.tar.gz; quit'
