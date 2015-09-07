11e5123a2b565df8b332d05d18cebac2e384c4db
laborc <laborc2009@gmail.com>
2015-09-07 17:45:14 +0800

    nodejs

--- docker-cmd/cs.py
+++ docker-cmd/cs.py
@@ -11,17 +11,13 @@ index = sys.argv[2]
 startPort = 42065 + 3*int(index)-2
 ip = str(45 + int(index))
 sshPort = str(startPort )
-memPort = str(startPort + 1)
-twPort = str(startPort + 2)
 
 #初始化实例
 executor = Executor('value-cs', {
     "ip": ip,
     "sshPort": sshPort,
     "portMap": {
-        "127.0.0.1:" + sshPort: "22",
-        memPort: "11211",
-        twPort: "22122"
+        "127.0.0.1:" + sshPort: "22"
     },
     "volumeMap": {
         "~/authorized_keys": "/root/.ssh/authorized_keys"
