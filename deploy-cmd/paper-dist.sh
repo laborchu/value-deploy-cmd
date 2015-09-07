f725eefaa38438b03bff7c10eb141db15392f4c3
yufengcai <yufengcai@yufengcai.yufengcai>
2015-09-08 01:15:38 +0800

    paper-dist

--- deploy-cmd/paper-dist.sh
+++ deploy-cmd/paper-dist.sh
@@ -1,2 +1,3 @@
 #!/bin/sh 
+rm /new_home/deploy_file/paper-server-1.0-SNAPSHOT.jar
 lftp -u devftp,devftp 61.153.97.56 -e 'get apps/paper-server-1.0-SNAPSHOT.jar -o /new_home/deploy_file/paper-server-1.0-SNAPSHOT.jar; quit'
