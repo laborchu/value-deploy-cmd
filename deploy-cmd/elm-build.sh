28092aaa2728bb51dfa6938a2d400be71039d4f7
hulb <laborc2009@gmail.com>
2015-10-23 16:57:07 +0800

    update build

--- deploy-cmd/elm-build.sh
+++ deploy-cmd/elm-build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh 
 rm -R /home/ftp/dev/apps/elm
 unzip -oq /home/ftp/dev/apps/elm-web.war -d /home/ftp/dev/apps/elm
+cp -R /home/ftp/dev/apps/elm_tmp/node_modules /home/ftp/dev/apps/elm/elm/
+cp -R /home/ftp/dev/apps/elm_tmp/bower_components /home/ftp/dev/apps/elm/elm/
 cd /home/ftp/dev/apps/elm/elm
-bower install
-cnpm install
+#bower install
+#npm install
 grunt build
 rm -R /home/ftp/dev/apps/elm/elm/node_modules
 cd /home/ftp/dev/apps
